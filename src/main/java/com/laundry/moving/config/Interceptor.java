e590c46e9d03ceac4c6f600ff93400c9d21c54ed
zhangzhen <1094705507@qq.com>
2019-12-01 13:17:02 +0800

    添加拦截器

---
+++ src/main/java/com/laundry/moving/config/Interceptor.java
@@ -0,0 +1,44 @@
+package com.laundry.moving.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class Interceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        String params = request.getQueryString();
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for (Cookie cookie: cookies) {
+                String name = cookie.getName();
+                String value = cookie.getValue();
+                System.out.println(name + " -----");
+                System.out.println(value + " +++++");
+            }
+        }
+        if(params != null && params.contains("zhangzhen") && !request.getRequestURL().equals("/index.html")) {
+            response.sendRedirect("/login");
+        }
+        System.out.println(">>>MyInterceptor1>>>>>>>在请求处理之前进行调用（Controller方法调用之前）");
+        return true;// 只有返回true才会继续向下执行，返回false取消当前请求
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        System.out.println(">>>MyInterceptor1>>>>>>>请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        System.out.println(">>>MyInterceptor1>>>>>>>在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行（主要是用于进行资源清理工作）");
+    }
+}
---
+++ src/main/java/com/laundry/moving/config/WebAppInterceptorConfig.java
@@ -0,0 +1,23 @@
+package com.laundry.moving.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebAppInterceptorConfig implements WebMvcConfigurer {
+
+    @Autowired
+    Interceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+//        registry.addInterceptor(interceptor).addPathPatterns("/**/*");
+        InterceptorRegistration interceptorRegistration = registry.addInterceptor(interceptor);
+        interceptorRegistration.addPathPatterns("/**/*");
+        interceptorRegistration.excludePathPatterns("/login");
+    }
+}
